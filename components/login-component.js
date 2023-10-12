78a28bea8b5ee3b568a172b55cdf479a2b97564f
christinnenko <christinnenko@yandex.ru>
2023-10-12 16:58:25 +0300

    add registration form

--- components/login-component.js
+++ components/login-component.js
@@ -1,43 +1,57 @@
 import { loginUser } from "../scripts/api.js";
 
 export function renderLoginComponent({ appEl, setToken, getFetchAndRender }) {
-    const appHTML = `<div class="container">
-    <div id="entrance-form" class="add-form">
-        <p class="add-form-title">Форма входа</p>
-        <input id="login" type="text" class="add-form-entrance" placeholder="Введите логин" />
-        <input id="password" type="password" class="add-form-entrance" placeholder="Введите пароль" />
-        <button id="entrance-button" class="add-form-button-entrance">Войти</button>
-        <div class="add-form-center">
-            <a class="add-form-link" href="#">Зарегистрироваться</a>
-        </div>
-    </div>`;
-    // отрисовка HTML строки на экране
-    appEl.innerHTML = appHTML;
 
-    document.getElementById('entrance-button').addEventListener('click', () => {
-        setToken('Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k');
-        const login = document.getElementById('login').value;
-        const password = document.getElementById('password').value;
+    let isLoginMode = true;
 
-        if (!login) {
-            alert('Введите логин');
-            return;
-        }
+    const renderForm = () => {
+        const appHTML = `<div class="container">
+        <div id="entrance-form" class="add-form">
+            <p class="add-form-title">Форма ${isLoginMode ? 'входа' : 'регистрации'}</p>
+            ${isLoginMode ? '' : `<input id="name" type="text" class="add-form-entrance" placeholder="Введите имя" />`}
+            
+            <input id="login" type="text" class="add-form-entrance" placeholder="Введите логин" />
+            <input id="password" type="password" class="add-form-entrance" placeholder="Введите пароль" />
+            <button id="entrance-button" class="add-form-button-entrance">${isLoginMode ? 'Войти' : 'Зарегистрироваться'}</button>
+            <div class="add-form-center">
+                <a id="toggle-entrance-link" class="add-form-link" href="#">Перейти ${isLoginMode ? 'к регистрации' : 'ко входу'}</a>
+            </div>
+        </div > `;
+        // отрисовка HTML строки на экране
+        appEl.innerHTML = appHTML;
 
-        if (!password) {
-            alert('Введите пароль');
-            return;
-        }
+        document.getElementById('entrance-button').addEventListener('click', () => {
+            setToken('Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k');
+            const login = document.getElementById('login').value;
+            const password = document.getElementById('password').value;
 
-        loginUser({
-            login: login,
-            password: password,
-        }).then((user) => {
-            setToken(`Bearer ${user.user.token}`);
-            getFetchAndRender();
-        }).catch(error => {
-            //TODO: выводить alert красиво
-            alert(error.message);
+            if (!login) {
+                alert('Введите логин');
+                return;
+            }
+
+            if (!password) {
+                alert('Введите пароль');
+                return;
+            }
+
+            loginUser({
+                login: login,
+                password: password,
+            }).then((user) => {
+                setToken(`Bearer ${user.user.token} `);
+                getFetchAndRender();
+            }).catch(error => {
+                //TODO: выводить alert красиво
+                alert(error.message);
+            });
         });
-    });
+
+        document.getElementById('toggle-entrance-link').addEventListener('click', () => {
+            isLoginMode = !isLoginMode;
+            renderForm();
+        });
+    }
+
+    renderForm();
 }
